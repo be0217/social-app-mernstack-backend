f9b20e543e0165211411980c8ded058fded6d15e
Ankit Kumar Sharma <43175867+CodesWithAnkit@users.noreply.github.com>
2020-01-04 19:47:10 +0530

    Adding Controller

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,62 +1,9 @@
 const express = require('express');
-const HttpError = require('../models/http-error');
+const placesController = require('../controllers/places-controllers');
 const router = express.Router();
 
-const DUMMY_PLACES = [
-  {
-    id: 'p1',
-    title: 'Empire State Building',
-    description: 'One of the most famous sky scrapers in the world!',
-    imageUrl:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
-    address: '20 W 34th St, New York, NY 10001',
-    location: {
-      lat: 40.7484405,
-      lng: -73.9878584
-    },
-    creator: 'u1'
-  },
-  {
-    id: 'p2',
-    title: 'Empire State Building',
-    description: 'One of the most famous sky scrapers in the world!',
-    imageUrl:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
-    address: '20 W 34th St, New York, NY 10001',
-    location: {
-      lat: 40.7484405,
-      lng: -73.9878584
-    },
-    creator: 'u2'
-  }
-];
+router.get('/:pid', placesController.getPlaceById);
 
-router.get('/:pid', (req, res, next) => {
-  const placeId = req.params.pid;
-
-  const place = DUMMY_PLACES.find(p => {
-    return p.id === placeId;
-  });
-
-  if (!place) {
-    throw new HttpError('Could Not find place for the provided place ID', 404);
-  }
-  res.json({ place });
-});
-
-router.get('/user/:uid', (req, res, next) => {
-  const userId = req.params.uid;
-
-  const place = DUMMY_PLACES.find(u => {
-    return u.creator === userId;
-  });
-
-  if (!place) {
-    return next(
-      new HttpError('Could Not find Place for the provided User ID', 404)
-    );
-  }
-  res.json({ place });
-});
+router.get('/user/:uid', placesController.getPlaceByUserId);
 
 module.exports = router;
